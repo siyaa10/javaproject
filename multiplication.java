61210f6e71cbd8ae1f2e05169ee86426cd6c806d
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-09-20 14:50:40 +0530

    MULTIPLICATION

---
+++ multiplication.java
@@ -0,0 +1,17 @@
+
+public class multiplication {
+	public static void main(String [] args)
+	{
+		int num1=10,num2=20;
+		int product=num1*num2;
+		System.out.print("product="+product);
+	}
+
+}
+
+
+
+
+
+
+
