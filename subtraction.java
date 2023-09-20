7a35c1a9ebb120e965205c6f78903ef53bfdf6c8
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-09-20 14:49:57 +0530

    SUBTRACTION

---
+++ subtraction.java
@@ -0,0 +1,16 @@
+
+public class subtraction {
+	public static void main(String [] args)
+	{
+		int num1=10,num2=20;
+		int difference=num1-num2;
+		System.out.print("difference="+difference);
+	}
+
+}
+
+
+
+
+
+
