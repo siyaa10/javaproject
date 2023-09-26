3ba9edea7a9cbfb764c15e13fe9d66402c952b32
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-09-26 16:17:23 +0530

    Add files via upload

---
+++ sumofpositivenum.java
@@ -0,0 +1,19 @@
+package sumofpositivenum.java;
+import java.util.Scanner;
+public class sumofpositivenum {
+	public static void main(String[]args) {
+		int number,sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the number");
+		number=sc.nextInt();
+		while(number>0)
+		{
+			sum=sum+number;
+			System.out.println("enter the next number");
+			number=sc.nextInt();
+
+		}
+		System.out.println("sum of the numbers is "+sum);
+	}
+
+}
