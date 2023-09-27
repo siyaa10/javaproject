31afc9ff8e780dab10bb26af81433bd8ec77139b
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-09-27 15:02:14 +0530

    Add files via upload

---
+++ natnumbers.java
@@ -0,0 +1,13 @@
+package natnumbers;
+
+public class natnumbers {
+	public static void main(String[]args) {
+		int i=0;
+		for(i=0;i<=10;i++)
+		{
+			System.out.println(i);
+		}
+		System.out.println("\n");
+	}
+
+}
