f80a5f78b1af8de58854cd8414c097dd78d2c4f6
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-10-03 14:52:39 +0530

    Add files via upload

---
+++ palindrome.java
@@ -0,0 +1,35 @@
+package palindrome.java;
+import java.util.Scanner;
+public class palindrome {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter string:");
+		String input=sc.next();
+		boolean isPalindrome=check(input);
+		if(isPalindrome)
+		{
+			System.out.println("the given string is palindrome");
+		}
+		else
+		{
+			System.out.println("the given string is not palindrome");
+		}
+	}
+		static boolean check(String input)
+		{
+			char[] charArray=input.toCharArray();
+			int length=input.length();
+			for(int i=0;i<length/2;i++)
+			{
+				if(charArray[i]!=charArray[length-i-1])
+				{
+					return false;
+				}
+			}
+			return true;
+		
+		
+	}
+
+}
