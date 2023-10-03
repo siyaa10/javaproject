17c557828cd4e029eb6ab6fb55741e053b1f382c
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-10-03 16:01:29 +0530

    Add files via upload

---
+++ frequencycount.java
@@ -0,0 +1,29 @@
+package frequencycount.java;
+import java.util.Scanner;
+public class frequencycount {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter a string");
+		String input=sc.nextLine();
+		System.out.println("enter the character to be checked");
+		char Checkme=sc.next().charAt(0);
+		int freq=checkFreq(input,Checkme);
+		System.out.println("number of occurances of the character:"+freq);
+		
+	}
+	static int checkFreq(String input,char Checkme)
+	{
+		char[]charArray=input.toCharArray();
+		int charCount=0;
+		for(int i=0;i<input.length();i++)
+		{
+			if(charArray[i]==Checkme)
+					{
+					charCount++;
+					}
+		}
+	
+return charCount;
+	}
+}
