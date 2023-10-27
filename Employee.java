79609ec56e06b8ec5859990477b99b3f76c330f2
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-10-27 14:38:47 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,37 @@
+package javalab;
+import java.util.Scanner;
+public class Employee{
+	public static void main(String[] args) {
+		Rectangle rectangle = new Rectangle();
+		Triangle triangle = new Triangle();
+		Hexagon hexagon = new Hexagon();
+        rectangle.numberOfSides();
+        triangle.numberOfSides();
+        hexagon.numberOfSides();
+	}
+
+}
+abstract class Shapes{
+	abstract public void numberOfSides();
+		
+}
+class Rectangle extends Shapes{
+	public void numberOfSides() {
+		System.out.println("Number of sides of Rectangle =4");
+		
+	}
+	
+}
+class Triangle extends Shapes{
+public void numberOfSides() {
+ System.out.println("Number of sides of Triangle = 3");
+	
+}
+}
+class Hexagon extends Shapes{
+	public void numberOfSides() {
+		System.out.println("Number of sides of Hexagon= 6");
+		
+	}
+}
+
