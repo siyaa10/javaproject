f273ace52ae1afa3776f006b92ff6b4c25a330cd
siyaa10 <145546548+siyaa10@users.noreply.github.com>
2023-11-17 14:18:04 +0530

    Add files via upload

---
+++ CustomException.java
@@ -0,0 +1,47 @@
+package javaproject;
+
+import java.util.Scanner;
+
+class Customer{
+int accountNumber;
+String accountType;
+String customerName;
+int balance;
+public void setCustomerDetails()
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter account number");
+accountNumber=sc.nextInt();
+System.out.println("Enter the account type");
+accountType=sc.next();
+System.out.println("Enter the customer name");
+customerName=sc.next();
+balance=sc.nextInt();
+}
+public void getCustomerDetails()
+{
+	System.out.println("Enter account number");
+	System.out.println("Enter account type");
+	System.out.println("Enter the customer name");
+	System.out.println();
+}
+public class CustomException {
+	public static void main(String[]args)
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number of customers");
+		int numOfCustomers
+		cust cust=new cust();
+		cust.set custdetails();
+		for(int i=0;i<numof customer;i++)
+			cut[i].Setcustdetails();
+	}
+	FOR(same)
+	{
+		getdetails
+	}
+			
+		
+	}
+
+}
